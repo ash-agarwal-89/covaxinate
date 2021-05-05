2dba53d3592b859f46fa5022abf0c9d786e471af
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-05 18:18:54 +1000

    ff

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 'use strict';
 
 const userAction = async () => {
-  try{
-  const response = await fetch('https://cors-anywhere.herokuapp.com/https://cdn-api.co-vin.in/api/v2/admin/location/districts/16', {
-    method: 'GET',
-    headers: {
-      'accept': 'application/json',
-      'Accept-Language': 'en_US'
-    }
+  
+  const response = await fetch('https://cdn-api.co-vin.in/api/v2/admin/location/districts/16', {
+    method: 'GET'
   });
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
   console.log(myJson);
-  
+  try{
     if(myJson){
       document.querySelector("h1").innerHTML = 'success';
     }else{
