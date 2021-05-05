89d7d6d875755778ac42c3aeb83739cf59900a7b
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-05 15:40:58 +1000

    trycatch

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const userAction = async () => {
+  try{
   const response = await fetch('https://cdn-api.co-vin.in/api/v2/admin/location/states', {
     method: 'GET',
     headers: {
@@ -11,7 +12,7 @@ const userAction = async () => {
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
   console.log(myJson);
-  try{
+  
     if(myJson){
       document.querySelector("h1").innerHTML = 'mmmm';
     }else{
