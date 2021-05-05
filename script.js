2888d64cf4491587b8c7e573795241dc98665c66
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-05 15:21:45 +1000

    updateHeader

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const userAction = async () => {
-  const response = await fetch('https://api.covid19api.com/countries');
+  const response = await fetch('https://api.covid19api.com/countries', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
   console.log(myJson);
