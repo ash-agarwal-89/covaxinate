687636cbf4726333e74ced5e45023daf4b947689
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-06 23:49:13 +1000

    subscribe link fix

--- myfooter.js
+++ myfooter.js
@@ -8,7 +8,7 @@ class MyFooter extends HTMLElement{
                     <li><a href="#" class="terms-of-use"><i class="fas fa-envelope gmail"></i> mail us</a></li>
                     <li><a href="#" class="feedback"><i class="fab fa-facebook facebook"></i> follow us</a></li>
                     <li><a href="#" class="terms-of-use"><i class="fab fa-twitter tweet"></i> tweet us</a></li>
-                    <li><a href="https://www.youtube.com/channel/UC3paSZA513norLu3b1sEOmA/about"><i class="fab fa-youtube utube"></i> subscribe</a></li>
+                    <li><a href="https://m.youtube.com/channel/UC3paSZA513norLu3b1sEOmA/featured"><i class="fab fa-youtube utube"></i> subscribe</a></li>
                 </ul>
                 <span class="powered-by">Powered by API Setu</span>
             </div>
