0c63bc165f91b6688622d22bb03801d89fca86c2
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-06 21:16:15 +1000

    check your site

---
+++ myfooter.js
@@ -0,0 +1,22 @@
+class MyFooter extends HTMLElement{
+
+    connectedCallback(){
+        this.innerHTML = `
+        <footer class="footer">
+            <div class="footer-items">
+                <ul class="footer-social-list">
+                    <li><a href="#" class="terms-of-use"><i class="fas fa-envelope gmail"></i> mail us</a></li>
+                    <li><a href="#" class="feedback"><i class="fab fa-facebook facebook"></i> follow us</a></li>
+                    <li><a href="#" class="terms-of-use"><i class="fab fa-twitter tweet"></i> tweet us</a></li>
+                    <li><a href="https://www.youtube.com/channel/UC3paSZA513norLu3b1sEOmA/about"><i class="fab fa-youtube utube"></i> subscribe</a></li>
+                </ul>
+                <span class="powered-by">Powered by API Setu</span>
+            </div>
+        </footer>`;
+    }
+
+    
+}
+
+
+customElements.define('my-footer', MyFooter);
---
+++ myheader.js
@@ -0,0 +1,17 @@
+class MyHeader extends HTMLElement{
+
+    connectedCallback(){
+        this.innerHTML = `
+        <header class="header">
+            <a href="/vaccine/vaccine.html" class="header_anchor">
+                <img src="../imgs/vaccine.png" alt="brand" class="brand_image">
+                <span class="brand_name">covaxinate</span>
+            </a>
+        </header>`;
+    }
+
+    
+}
+
+
+customElements.define('my-header', MyHeader);
