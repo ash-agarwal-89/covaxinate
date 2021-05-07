4b868f65b09ed5e33db7a869fdf130c9bdace1c9
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-08 01:51:54 +1000

    fixes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let readBtn = document.querySelector('.viewmore-btn');
 let appointBtn = document.getElementById('getstarteButton');
 let contents = document.querySelector('.content-text');
 let appointmentContainer = document.querySelector('.appointment-container');
+let searchApptBtn = document.querySelector(".fetch-appointments-btn");
 let testFlag = false;
 
 
@@ -20,8 +21,9 @@ document.addEventListener('click', (event)=>{
         fetchStates();
 
     }
-    if(event.target.id == "state"){
+    if(event.target.id == "fetch-appointment"){
         console.log(event.target);
+        fetchAppointments();
 
     }
 })
@@ -93,6 +95,7 @@ async function fetchStates(){
 
 async function fetchDistrcits(){
     try{
+        searchApptBtn.disabled = true;
         let selectedState = document.getElementById("state").value;
         console.log(selectedState);
         let resjson = '';
@@ -153,7 +156,84 @@ async function fetchDistrcits(){
 async function onDistrictSelection(){
     let selecteddist = document.getElementById("district").value;
     console.log(selecteddist)
+    searchApptBtn.disabled = false;
+
 
 }
 
+
+async function fetchAppointments(){
+    try{
+        let selectedState = document.getElementById("state").value;
+        let selectedDistrict = document.getElementById("district").value;
+        console.log(selectedDistrict);
+        let resjson = '';
+        if(testFlag){
+            let resjsonStr = `{
+                "centers": [
+                  {
+                    "center_id": 1234,
+                    "name": "District General Hostpital",
+                    "name_l": "",
+                    "address": "45 M G Road",
+                    "address_l": "",
+                    "state_name": "Maharashtra",
+                    "state_name_l": "",
+                    "district_name": "Satara",
+                    "district_name_l": "",
+                    "block_name": "Jaoli",
+                    "block_name_l": "",
+                    "pincode": "413608",
+                    "lat": 28.7,
+                    "long": 77.1,
+                    "from": "09:00:00",
+                    "to": "18:00:00",
+                    "fee_type": "Free",
+                    "vaccine_fees": [
+                      {
+                        "vaccine": "COVISHIELD",
+                        "fee": "250"
+                      }
+                    ],
+                    "sessions": [
+                      {
+                        "session_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        "date": "31-05-2021",
+                        "available_capacity": 50,
+                        "min_age_limit": 18,
+                        "vaccine": "COVISHIELD",
+                        "slots": [
+                          "FORENOON",
+                          "AFTERNOON"
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }`;
+              resjson = JSON.parse(resjsonStr);
+
+        }else{
+
+            const res = await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=512&date=31-03-2021`, {
+                method: 'GET'
+              });
+            resjson = await res.json();
+        }
+        let responseHtml = document.querySelector('.response');
+        if(resjson){
+            responseHtml.innerHTML = 'success appt retrieved'+JSON.stringify(resjson);
+        }else{
+            responseHtml.innerHTML = 'failed';
+        }
+        
+    
+        console.log('ok');
+    }catch(err){
+        let responseHtml = document.querySelector('.response');
+        console.log('error ...',err);
+        // responseHtml.innerHTML = err;
+    }
+
+}
 
