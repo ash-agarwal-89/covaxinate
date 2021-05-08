22834bd2b0f59339c205131919a13f49951aa310
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-08 21:23:04 +1000

    test = false

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let bgLoaderAppt = document.querySelector('.bg-loader-appointment');
 
 let closeBtn = document.getElementById('close-btn');
 let mainContent =  document.querySelector('.main');
-let testFlag = true;
+let testFlag = false;
 
 // document.addEventListener('contextmenu', function(e) {
 //   if(!testFlag)
