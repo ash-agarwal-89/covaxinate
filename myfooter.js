5085534cae0b03051cf6e57b598556b29c7937c4
ash-agarwal-89 <70432105+ash-agarwal-89@users.noreply.github.com>
2021-05-09 01:33:32 +1000

    social links

--- myfooter.js
+++ myfooter.js
@@ -5,9 +5,9 @@ class MyFooter extends HTMLElement{
         <footer class="footer">
             <div class="footer-items">
                 <ul class="footer-social-list">
-                    <li><a href="#" class="terms-of-use"><i class="fas fa-envelope gmail"></i> mail us</a></li>
+                    <li><a href="mailto:myapps.amplified@gmail.com" class="terms-of-use"><i class="fas fa-envelope gmail"></i> mail us</a></li>
                     <li><a href="#" class="feedback"><i class="fab fa-facebook facebook"></i> follow us</a></li>
-                    <li><a href="#" class="terms-of-use"><i class="fab fa-twitter tweet"></i> tweet us</a></li>
+                    <li><a href="https://twitter.com/covaxinate" class="terms-of-use"><i class="fab fa-twitter tweet"></i> tweet us</a></li>
                     <li><a href="https://m.youtube.com/channel/UC3paSZA513norLu3b1sEOmA/featured"><i class="fab fa-youtube utube"></i> subscribe</a></li>
                 </ul>
                 <span class="powered-by">Powered by API Setu</span>
